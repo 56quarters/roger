e5f2ba7fc49966b2502927f30841178180b823c2
Nick Pillitteri <nick@tshlabs.org>
2020-11-20 21:02:23 -0500

    Start passing messages back and forth

--- roger.go
+++ roger.go
@@ -1,7 +1,14 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	log "github.com/sirupsen/logrus"
+	"sync"
+)
+
+const (
+	BufSize = 100
 )
 
 type HealthCheck interface {
@@ -9,19 +16,51 @@ type HealthCheck interface {
 }
 
 type CpuHealthCheck struct {
-
 }
 
 func NewCpuHealthCheck() CpuHealthCheck {
 	return CpuHealthCheck{}
 }
 
-func (c CpuHealthCheck) Refresh() (map[string]string, error) {
+func (c *CpuHealthCheck) Refresh() (map[string]string, error) {
 	return map[string]string{"status": "ok"}, nil
 }
 
+func SendResults(check HealthCheck, queue chan<- map[string]string, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	res, err := check.Refresh()
+	if err != nil {
+		log.Warn("Couldn't refresh check results: %s", err)
+	} else {
+		queue <- res
+	}
+}
+
+func PrintCheckResults(queue <-chan map[string]string, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	res := <-queue
+	bytes, err := json.Marshal(res)
+	if err != nil {
+		log.Warn("Couldn't serialize result to JSON: %s", err)
+	} else {
+		fmt.Printf("Roger roger, over and out: %s", string(bytes))
+	}
+}
+
 func main() {
+	log.SetFormatter(&log.JSONFormatter{})
+	var wg sync.WaitGroup
+
 	check := NewCpuHealthCheck()
-	check.Refresh()
-	fmt.Println("Roger roger, over and out")
+	queue := make(chan map[string]string, BufSize)
+
+	wg.Add(1)
+	go SendResults(&check, queue, &wg)
+
+	wg.Add(1)
+	go PrintCheckResults(queue, &wg)
+
+	wg.Wait()
 }
