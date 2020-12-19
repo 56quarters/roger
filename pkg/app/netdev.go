dfb7a59890349ba52a929bee00520ef1a566a72a
Nick Pillitteri <nick@tshlabs.org>
2020-12-19 14:54:26 -0500

    License header in files

--- pkg/app/netdev.go
+++ pkg/app/netdev.go
@@ -1,3 +1,13 @@
+// Roger - DNS and network metrics exporter for Prometheus
+//
+// Copyright 2020 Nick Pillitteri
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
 package app
 
 // read network stats from /proc
